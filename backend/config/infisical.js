632ba94a8c7cb20cb7e7904c5b3c9282e0a8b4b4
SaisagarYT <saisagarsylada@gmail.com>
2026-06-12 13:45:16 +0530

    updated README.md

--- backend/config/infisical.js
+++ backend/config/infisical.js
@@ -2,4 +2,15 @@ import {InfisicalSDK} from '@infisical/sdk'
 
 const client = new InfisicalSDK();
 
+try{
+    const response = await client.auth().universalAuth.login({
+        clientId:process.env.INFISICAL_CLIENT_ID,
+        clientSecret:process.env.INFISICAL_CLIENT_SECRET
+    })
+    console.log(response.auth)
+}
+catch(err){
+    console.error(err);
+}
+
 const secret = client.secrets
